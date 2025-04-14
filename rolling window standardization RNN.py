0d6f1dfce537eb2364e49535e61d9e76390f4553
mcar18 <84042230+mcar18@users.noreply.github.com>
2025-04-14 14:28:40 -0400

    longer training time

--- rolling window standardization RNN.py
+++ rolling window standardization RNN.py
@@ -15,7 +15,7 @@ from tensorflow.keras.layers import LSTM, Dropout, Dense
 # -----------------------------
 tickers = ['SPY', 'AAPL', 'MSFT'] # , 'MA', 'V', 'AXP', 'CRM', 'GOOG', 'NVDA', 'PLTR', 'TSLA']  # Expand as needed
 start_date = '2010-01-01'
-end_date   = '2022-12-31'
+end_date   = '2025-04-11'
 sequence_length = 60      # Number of prior days used as input
 forecast_horizon = 60     # Predict next 60 days (for multi-step)
 output_root = 'RNN forecasts'
